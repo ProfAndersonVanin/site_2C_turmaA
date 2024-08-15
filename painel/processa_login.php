7bd67ff2e79b355e22d5310f7b91aa7b270c340a
Anderson Vanin <53703505+ProfAndersonVanin@users.noreply.github.com>
2024-08-15 12:34:20 -0300

    Add files via upload

--- painel/processa_login.php
+++ painel/processa_login.php
@@ -6,13 +6,22 @@
     $consulta = "SELECT * FROM usuarios WHERE email = '$email' AND senha = '$senha'";
 
     $resultado = $conexao->query($consulta);
+
+    //echo "Numeros de registros ==> ". $resultado;
+    //var_dump($resultado);
+
+
     $registros = $resultado->num_rows;
+
+    //echo "Numeros de registros ==> ". $registros;
+
     $resultado_usuario = mysqli_fetch_assoc($resultado);
     
     //var_dump($resultado_usuario);
 
     if($registros == 1){
-        //echo "TE ACHEI";       
+        //echo "TE ACHEI ==> " . $resultado_usuario['nome'];       
+               
         header('Location: index.php');
 
     }
